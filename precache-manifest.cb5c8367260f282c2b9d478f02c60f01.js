904103e4cbfa60716687e6275de034c3ea3ac243
Yuzeix 2.0 <vladimir.ygl@gmail.com>
2023-05-21 06:49:22 -0300

    test

---
+++ precache-manifest.cb5c8367260f282c2b9d478f02c60f01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aae6f311fd36475ca17",
+    "url": "/Project-TODOs-React.js/static/css/main.f569735f.chunk.css"
+  },
+  {
+    "revision": "1aae6f311fd36475ca17",
+    "url": "/Project-TODOs-React.js/static/js/main.1aae6f31.chunk.js"
+  },
+  {
+    "revision": "6fdb30686cb6e95baab6",
+    "url": "/Project-TODOs-React.js/static/js/1.6fdb3068.chunk.js"
+  },
+  {
+    "revision": "d812955edcf4baed5312",
+    "url": "/Project-TODOs-React.js/static/js/runtime~main.d812955e.js"
+  },
+  {
+    "revision": "996d2deebe2748d2bc6132cb5899c114",
+    "url": "/Project-TODOs-React.js/index.html"
+  }
+];
